7c86e15442a47ca6901b4e93f64fe7e72c093236
Jasper Wong <wlcjasper@gmail.com>
2024-09-21 13:17:19 +0100

    Export out the create post function

--- src/app/writesqlite/page.tsx
+++ src/app/writesqlite/page.tsx
@@ -1,22 +1,21 @@
-import { revalidatePath } from 'next/cache';
+import { revalidatePath } from "next/cache"
 import { prisma } from "@/lib/prisma"
 
-const createPost = async (formData: FormData) => {
-  'use server';
+export async function createPost(formData: FormData) {
+  "use server"
 
-  const name = formData.get('name') as string;
+  const name = formData.get("name") as string
 
   await prisma.post.create({
     data: {
       name,
     },
-  });
+  })
 
-  revalidatePath('/');
-};
+  revalidatePath("/")
+}
 
 const Home = async () => {
-
   return (
     <div className="p-4 flex flex-col gap-y-4">
       <h2>Home</h2>
@@ -25,9 +24,8 @@ const Home = async () => {
         <input type="text" name="name" placeholder="Name" />
         <button type="submit">Create</button>
       </form>
-
     </div>
   )
-};
+}
 
-export default Home;
+export default Home
