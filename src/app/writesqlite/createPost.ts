e4b42db414888416370b30c0dca62a5744a8167b
Jasper Wong <wlcjasper@gmail.com>
2024-09-21 13:26:57 +0100

    Factor out createPost

---
+++ src/app/writesqlite/createPost.ts
@@ -0,0 +1,15 @@
+import { revalidatePath } from "next/cache"
+import { prisma } from "@/lib/prisma"
+
+export async function createPost(formData: FormData) {
+  "use server"
+  const name = formData.get("name") as string
+
+  await prisma.post.create({
+    data: {
+      name,
+    },
+  })
+
+  revalidatePath("/")
+}
--- src/app/writesqlite/page.tsx
+++ src/app/writesqlite/page.tsx
@@ -1,19 +1,4 @@
-import { revalidatePath } from "next/cache"
-import { prisma } from "@/lib/prisma"
-
-export async function createPost(formData: FormData) {
-  "use server"
-
-  const name = formData.get("name") as string
-
-  await prisma.post.create({
-    data: {
-      name,
-    },
-  })
-
-  revalidatePath("/")
-}
+import { createPost } from "./createPost"
 
 const Home = async () => {
   return (
