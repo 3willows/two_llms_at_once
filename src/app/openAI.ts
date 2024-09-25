5448fedad6b8dcd3a09a49cffa9009e49a9081a1
Jasper Wong <wlcjasper@gmail.com>
2024-09-25 12:02:59 +0100

    Dip my toe in stream text

--- src/app/openAI.ts
+++ src/app/openAI.ts
@@ -1,6 +1,7 @@
 "use server"
 import { createOpenAI } from "@ai-sdk/openai"
 import { generateText } from "ai"
+import { streamText } from 'ai';
 import { saveOpenAI } from "./saveOpenAI"
 
 const openai = createOpenAI({
@@ -12,6 +13,15 @@ export async function AskOpenAI(promptFromUser: string) {
     model: openai("gpt-4-turbo"),
     prompt: promptFromUser,
   })
+
+  const { textStream } = await streamText({
+    model: openai('gpt-4-turbo'),
+    prompt: promptFromUser,
+  });
+  
+  for await (const textPart of textStream) {
+    console.log(textPart);
+  }
 
   await saveOpenAI(text)
 
