63cab379c6df7801dbacd270bb877af6ba021057
Jasper Wong <wlcjasper@gmail.com>
2024-09-30 11:42:22 +0100

    Insert something every time you load

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,22 @@
-import { createClient } from "../../../utils/supabase/server";
-  
-  export default async function Notes() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+import { createClient } from "../../../utils/supabase/server"
+
+export default async function Notes() {
+  const supabase = createClient()
+
+  const { data: notes } = await supabase.from("notes").select()
+
+  const { data, error } = await supabase
+    .from("notes")
+    .insert({title: "Denmark" })
+    .select()
+
+    if (error) {
+      console.error("Error inserting data:", error)
+      // Handle the error appropriately
+    } else {
+      console.log("Inserted data:", data)
+      // Use the inserted data as needed
+    }
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
